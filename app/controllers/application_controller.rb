9dfb0fe183fbfc12def7f4273646d393906e6826
El-paolo <paolocordovacontacto@gmail.com>
2023-09-03 19:58:07 -0300

    added the models and edited the devise parameter sanitizers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, images_attributes: [:url,
+        :context, :id]])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, images_attributes:
+        [:url, :context, :id]])
+        end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  accepts_nested_attributes_for :images
   has_many :images, as: :imageable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
