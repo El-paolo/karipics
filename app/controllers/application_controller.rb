132ec5b6ae574564403de34963355ca26d6e79e4
El-paolo <paolocordovacontacto@gmail.com>
2023-09-03 20:43:41 -0300

    fixed some bugs in the user model and edited devise sanitizers, added views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    protected
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, images_attributes: [:url,
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, images_attributes: [:url,
         :context, :id]])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, images_attributes:
+        devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, images_attributes:
         [:url, :context, :id]])
         end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  accepts_nested_attributes_for :images
   has_many :images, as: :imageable
+  accepts_nested_attributes_for :images
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
