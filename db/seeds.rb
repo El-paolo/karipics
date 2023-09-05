1db79ae5438aebb4294444bad4ea02604de510b1
El-paolo <paolocordovacontacto@gmail.com>
2023-09-05 13:00:49 -0300

    render configs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+#este seed nos crea el usuario admin
+
+user = User.new(
+    username: "Kari"
+    email: "test1@gmail.com",
+    password: "asdasdasd",
+    password_confirmation: "asdasdasd",
+    role: 1
+)
+
+# Genera la URL de la imagen con Faker
+image_url = Faker::Avatar.image(slug: "user-1", size: "50x50", format: "jpg")
+
+# Abre y adjunta la imagen desde la URL
+user.image.attach(
+    io: open(image_url),
+    filename: "avatar_user_1.jpg",
+    content_type: 'image/jpeg'
+)
+
+user.save!
+
+
