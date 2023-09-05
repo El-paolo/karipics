05d1e52dc702e4f2df6ecb7446f5a0ad7d2200a6
El-paolo <paolocordovacontacto@gmail.com>
2023-09-04 23:40:19 -0300

    added the comments form and nested them to posts, pictures and some styles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,6 +8,8 @@ class PostsController < ApplicationController
 
   # GET /posts/1 or /posts/1.json
   def show
+    @comment = Comment.new
+    @comments = @post.comments 
   end
 
   # GET /posts/new
@@ -22,6 +24,7 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user = current_user 
 
     respond_to do |format|
       if @post.save
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   #has_many :images, as: :imageable
   has_one_attached :images
+  has_many :comments, dependent: :destroy
 end
